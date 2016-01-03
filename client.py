8bbe66226701646a2e840dfb72976236701aebe9
Rohan Yadav <rohany@andrew.cmu.edu>
2016-01-03 18:45:40 -0500

    Added the comments

--- client.py
+++ client.py
@@ -1,28 +1,66 @@
 import socket
 import sys
 from _thread import *
+
+#look at this function later
 def servGet():
+    #as we started this function in a thread, it is going at the same time as the rest of the code
+    #we start a while true loop here to constantly read from the server
     while True:
+        #we get the message from the server with the recv function
+        #the argument is the size of the message that is being recieved
+        #and since we encoded the message when we sent it, we must decode it when we recieve it
+        #recv is a blocking function, meaning the code will not go past this function
+        #until some message is recieved
         msg = s.recv(1024).decode()
+        #we only want to print out actual messages, so we filter out any empty strings
         if msg != "":
             print(msg, "\n")
+        #now go back down past the thread creation
 
+#Here we create the socket for the client
 s = socket.socket()
 
+#As this code is running on the local host, we get the host socket, or the server as such
 host = socket.gethostname()
 
-port = int(sys.argv[1])
-name = sys.argv[2]
+#Here we let the user enter the port they are using when they run this file
+#we must check that there are enough arguments
+#sys.argv is the command line argument list
+if len(sys.argv) == 3:
+    #Get the port as an integer from the arguments
+    port = int(sys.argv[1])
+    #Get the users name from the arguments
+    name = sys.argv[2]
+else:
+    print("Usage: client.py port name")
+
+#now connect the client socket to the host socket
 s.connect((host, port))
+
+#send the user's name to the server first
+#note you must encode the string for the socket to send it in Python 3
 s.send(name.encode())
+
+#Now to recieve data from the server we must start a new thread
+#A thread is a seperate process that the program runs at the same time
+#We start a thread so that we can read from the server, and send to the server at the same time
+#The usage is start_new_thread(FunctionName, argument tuple)
+#check out the servGet function now
 thread = start_new_thread(servGet,())
 
+#Here we start the sending from the client to the server
 while True:
-  
-  msg = input()
-  if msg == "exit":
-      s.close()
-      sys.exit()
-  s.send(msg.encode())
+    #get the typed input from the user
+    msg = input()
+    #I decided to add if the user types exit, we end this client socket
+    if msg == "exit":
+        #first we must close the socket so no more communication occurs
+        s.close()
+        #then we exit from the program
+        #the thread we created exits as well
+        sys.exit()
+    #assuming the message is not "exit", we send the message to the server
+    s.send(msg.encode())
 
 
--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-import select
 import socket
 import sys
 import os
@@ -6,44 +5,87 @@ from threading import Thread
 from _thread import *
 
 
-
+#look at this function later
 def read(c, name, cliList, s, started):
+    #for our read function, we want to know the client this thread is going to handle,
+    #the name of the client
+    #all the current clients, the host, and whether the program was started or not
+    #we use a while true to continually recieve
     while True:
+        #here we use recv to recieve any message sent from a client
         msg = c.recv(1024).decode()
+        #an empty string sent by a client is what is sent when the client has closed the socket
+        #so we need to catch this case as such
         if(msg == ""):
+            #when the client has closed the socket on thier end, we must close it on the server 
+            #end as well
             c.close()
+            #then we remove this client from our list of active clients
             cliList.remove(c)
+            #additionally, we want the server to close when it has no more clients
+            #so if the client list is empty, we will end the server program
             if len(cliList) == 0 and started:
+                #end the server program
                 os._exit(0)
             break
         else:
+            #assuming the message is not the empty string, we print the message out in the server
             print(name,":",msg)
+            #and then send the message to each client
             for cli in cliList:
+                #we iterate through all the clients, and send the message to every client
+                #that is not the one who sent it
                 if cli is not c:
                     msg = name + ": " + msg
                     cli.send(msg.encode())
 
+#Here we create the socket for the server, or the host
 s = socket.socket()
+#As we are on the local host, we get the host name of the machine with the following command
 host = socket.gethostname()
-port = int(sys.argv[1])
+#set the port to get it from the command line
+port = 0;
+#We want to get arguments from the command line, so we need to check if the correct number is there
+if len(sys.argv) == 2:
+    #if we have the correct number, we get the port number we want to use as an integer
+    port = int(sys.argv[1])
+else:
+    #handle when input is incorrect
+    print("Usage: server.py port")
 
+#now that we have the host and the port, we bind the socket to the host and port number
+#letting it act as the server
 s.bind((host,port))
-
+#now we listen for clients
+#the argument 5 is the maximum number of connect requests
 s.listen(5)
+
+#here we make some lists keeping track of the data we need to make a simple server
+#a list of the names that the clients have
 nameList = []
+#a list of the clients
 cliList = []
-addrList = []
-threadList = []
+#and a variable keeping track of whether the program has started yet
 started = False
-while True:
-       
+#here is the body of the program, running the server
+while True: 
+    #to let a client connect, we use the accept() method to get the client and the address
+    #like recv, this function also blocks, in that the code wont go on until a client connects
     c, addr = s.accept()
+    #since we had the client send their name first, we recieve the name, and add it to our list
     nameList.append(c.recv(1024).decode())
+    #then we add the new client to our list as well
     cliList.append(c)
     
+    #as we have at least 1 client connected, we can set the started variable to true
     started = True
-    thread = start_new_thread(read, (c, nameList[-1], cliList,s,started))
-    threadList.append(thread)
+    #now since we want to read from all the clients, and send and recieve messages, we need a new thread
+    #so now go up and look at the function read
+    #we give the function the arguments it needs
+    #the current client to handle, its name
+    #we use nameList[-1] as -1 index is the last index, and since we just added it, the 
+    #name we want is the last index
+    thread = start_new_thread(read, (c, nameList[-1], cliList, s, started))
 
 
 
